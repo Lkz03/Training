ea4d7165386bac191b2196f9236557b2909adcda
Andrius Bogda <AndriusBogda@coherentsolutions.com>
2023-01-25 17:18:27 +0200

    added validation for plan object

---
+++ Training/Objects/Matrix.cs
@@ -0,0 +1,9 @@
+﻿namespace Training.Objects
+{
+    public class Matrix<T>
+    {
+        private T[][] _matrix;
+        
+        public Matrix(T[][] matrix) => _matrix = matrix;
+    }
+}
--- Training/Objects/Plan.cs
+++ Training/Objects/Plan.cs
@@ -1,21 +1,57 @@
-﻿namespace Training.Objects
+﻿using Training.Objects.Enums;
+
+namespace Training.Objects
 {
     public class Plan
     {
+        private const decimal _maxAmount = 999999999.99M;
+        private const decimal _minAmount = -999999999.99M;
+
+        private decimal _contributionAmount;
+        private PriorTaxYear _priorTaxYear;
+
         public int EmployeeIdentifier { get; set; }
         public DateOnly ContributionDate { get; set; }
         public string ContributionDescription { get; set; }
-        public decimal ContributionAmount { get; set; }
-        public string PlanName { get; set; }
-        public string PriorTaxYear { get; set; }
+        public decimal ContributionAmount
+        { 
+            get => _contributionAmount;
 
-        public Plan(string id, string date, string desc, string amount, string name, string taxYear)
+            set
+            {
+                if (value < 0 && PlanName.ToLower().Contains("hsa"))
+                {
+                    throw new ArgumentException("Value may not be negative");
+                }
+                else if (value > _maxAmount || value < _minAmount)
+                {
+                    throw new ArgumentException("Value is out of bounds");
+                }
+
+                _contributionAmount = value;
+            }
+        }
+        public string PlanName { get; set; }
+        public PriorTaxYear? PriorTaxYear
+        {
+            get => _priorTaxYear;
+
+            set
+            {
+                if (!PlanName.ToLower().Contains("hsa"))
+                {
+                    throw new ArgumentException("Only valid for HSA");
+                }
+            }
+        }
+
+        public Plan(string id, string date, string desc, string amount, string name, PriorTaxYear taxYear)
         {
             EmployeeIdentifier = Int32.Parse(id);
             ContributionDate = DateOnly.Parse(date.Insert(2, "-").Insert(5, "-"));
             ContributionDescription = desc;
-            ContributionAmount = Decimal.Parse(amount);
             PlanName = name;
+            ContributionAmount = Decimal.Parse(amount);
             PriorTaxYear = taxYear;
         }
     }
